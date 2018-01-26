664ecb6d485a07330b3dbc670a637107f47bbacb
Daniel Devitt <ddevitt@uboonegpvm02.fnal.gov>
2018-01-26 13:46:21 -0600

    Adding TwoTrackCheck algorithm

---
+++ Algorithms/TwoTrackCheck.cxx
@@ -0,0 +1,68 @@
+#include "TwoTrackCheck.h"
+
+bool TwoTrackCheck(art::Event &evt, bool checkMIPs)
+{
+   art::Handle<std::vector<ubana::SelectionResult>> selection_h;
+   evt.getByLabel("UBXSec", selection_h);
+   if(!selection_h.isValid()){
+      mf::LogError(__PRETTY_FUNCTION__) << "SelectionResult product not found." << std::endl;
+      throw std::exception();
+   }
+
+   //Get TPCObject
+   art::FindManyP<ubana::TPCObject> tpcobject_from_selection(selection_h, evt, "UBXSec");
+   if(tpcobject_from_selection.at(0).size() == 0) return false; //No TPCObject
+   art::Ptr<ubana::TPCObject> tpcobj_candidate = tpcobject_from_selection.at(0).at(0);
+   art::Handle<std::vector<ubana::TPCObject>> tpcobj_h;
+   evt.getByLabel("TPCObjectMaker", tpcobj_h);
+
+   //Get PFPs (in TPCObject)
+   art::FindManyP<recob::PFParticle> pfps_from_tpcobject(tpcobj_h, evt, "TPCObjectMaker");
+   std::vector<art::Ptr<recob::PFParticle>> pfps = pfps_from_tpcobject.at(tpcobj_candidate.key());
+
+   //Find neutrino
+   size_t nuID = -1;
+   for (auto pfp : pfps) {
+      if(lar_pandora::LArPandoraHelper::IsNeutrino(pfp)) {
+         nuID = pfp.Self();
+         break;
+      }
+   }
+
+   if(nuID == -1) {
+      mf::LogError(__PRETTY_FUNCTION__) << "No neutrino PFP in TPCObject" << std::endl;
+      throw std::exception();
+   }
+
+   //Find track-like daughters of the neutrino
+   std::vector<recob::PFParticle> daughter_track_pfps;
+   for (auto pfp : pfps) {
+      if(lar_pandora::LArPandoraHelper::IsTrack(pfp) && pfp.Parent()==nuID) {
+         daughter_track_pfps.emplace_back(pfp);
+      }
+   }
+
+   if(daughter_track_pfps.size() < 2) return false;
+
+   else if(!checkMIPs) return true;
+
+   else {
+      //Get PFPs (in event)
+      art::Handle<std::vector<recob::PFParticle> > pfp_h;
+      evt.getByLabel("pandoraNu::UBXSec",pfp_h);
+      if(!pfp_h.isValid()){
+         mf::LogError(__PRETTY_FUNCTION__) << "PFP product not found." << std::endl;
+         throw std::exception();
+      }
+
+      art::FindManyP<recob::Track> tracks_from_pfps(pfp_h, evt, "pandoraNu");
+
+      int MIPs = 0;
+      for (auto pfp : daughter_track_pfps) {
+         if(IsMIP(tracks_from_pfps.at(pfp.key()), evt)) MIPs++;
+      }
+
+      if(MIPs >= 2) return true;
+      else return false;
+   }
+}
---
+++ Algorithms/TwoTrackCheck.h
@@ -0,0 +1,18 @@
+// Marco's code includes
+#include "uboone/UBXSec/DataTypes/TPCObject.h"
+
+// larsoft includes
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "larpandora/LArPandoraInterface/LArPandoraHelper.h"
+
+// art includes
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include "canvas/Persistency/Common/FindManyP.h"
+
+// CC1pi method includes
+#include "uboone/CC1pi/Algorithms/MIPConsistencyCheck_Marco.h"
+
+bool TwoTrackCheck(art::Event &evt, bool checkMIPs);
