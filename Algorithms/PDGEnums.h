71aa3b64e91a1b2ed265590fdf8ebf9ecb94b5dd
kduffy <kduffy@fnal.gov>
2018-02-09 11:43:32 -0600

    Enums for PDG code to particle type

---
+++ Algorithms/PDGEnums.h
@@ -0,0 +1,28 @@
+#ifndef PDGENUMS
+#define PDGENUMS
+
+enum PDGCode{
+    kNuMu     = 14,
+    kNuMuBar  = -14,
+    kNuE      = 12,
+    kNuEBar   = -12,
+    kNuTau    = 16,
+    kNuTauBar = -16,
+    kMuMinus  = 13,
+    kMuPlus   = -13,
+    kPiMinus  = -211,
+    kPiPlus   = 211,
+    kPiZero   = 111,
+    kElectron = 11,
+    kPositron = -11,
+    kTauMinus = 15,
+    kTauPlus  = -15,
+    kPhoton   = 22,
+    kProton   = 2212,
+    kNeutron  = 2112,
+    kArgon    = 1000180400,
+    kBindino  = 2000000101
+};
+
+#endif
+
