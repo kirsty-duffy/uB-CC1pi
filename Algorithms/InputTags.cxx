0067ab479e7a42b7131557352cb63f3c307add03
kduffy <kduffy@fnal.gov>
2018-05-23 14:42:51 -0500

    Add Chi2PIDlabel to input tags and fcl files, and add other needed input tags to CC1pi.fcl and CC1pi_data.fcl

--- Algorithms/InputTags.cxx
+++ Algorithms/InputTags.cxx
@@ -4,4 +4,5 @@ InputTags::InputTags(fhicl::ParameterSet const &p){
    fTrackLabel       = p.get<std::string>("TrackLabel");
    fCalorimetryLabel = p.get<std::string>("CalorimetryLabel");
    fSelectionLabel   = p.get<std::string>("SelectionLabel");
+   fPIDLabelChi2     = p.get<std::string>("ParticleIdChi2Label");
 }
