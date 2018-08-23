8e1ae60025155e4e496fb39b5076e9f66f7d8a86
kduffy <kduffy@fnal.gov>
2018-08-23 09:51:06 -0500

    Refactoring to allow us to give a custom vector of MIP cuts (for efficiency/purity plots)

--- PlottingTools/CC1pi_MIPcut.cxx
+++ PlottingTools/CC1pi_MIPcut.cxx
@@ -9,40 +9,52 @@
 // -------------------------------------------------- //
 
 // Definition of MIP cut
-double EvalMIPCut(treevars *vars, int i_tr){
-
-   double isMIP=1;
-
-   // Get MIP cuts
-   std::vector<CC1piPlotVars> MIPCutVars = GetMIPCutVars(vars);
-
+double doEvalMIPCut(std::vector<CC1piPlotVars> MIPCutVars, treevars *vars, int i_tr){
    // Loop over MIP cuts and apply each one to the track
    for (size_t i_mipcut=0; i_mipcut < MIPCutVars.size(); i_mipcut++){
+
       double value = MIPCutVars.at(i_mipcut).Var->at(i_tr);
-      double cutval = MIPCutVars.at(i_mipcut).CutValue;
       bool MIPlow = MIPCutVars.at(i_mipcut).KeepBelowCut;
+      double cutval = MIPCutVars.at(i_mipcut).CutValue;
+
+      // std::cout << " MIP cut " << i_mipcut << " cutval " << cutval << "(KeepBelowCut = " << MIPlow << ")" ", value = " << value << std::endl;
 
       if (value == -9999 || value == -999) {
-         isMIP=-9999;
-         break;
+         return -9999;
       }
       else if (!std::isnormal(value)){
          // std::cout << "[WARNING :: CC1pi_MIPcut.cxx] not-normal value for " << MIPCutVars.at(i_mipcut).histtitle << std::endl;
-         isMIP=-9999;
-         break;
+         return -9999;
       }
       else if (MIPlow && (value > cutval)){
-         isMIP=0;
-         break;
+         // std::cout << "value > cutval so fails" << std::endl;
+         return 0;
       }
       else if (!MIPlow && (value < cutval)){
-         isMIP=0;
-         break;
+         // std::cout << "value < cutval so fails" << std::endl;
+         return 0;
       }
-
-      // std::cout << i_tr << ": " << value << "  " << cutval << "  " << isMIP << std::endl;
    }
-   return isMIP;
+   // If it makes it all the way to the end of the loop, it must have passed every cut. Return 1.
+   return 1;
+};
+
+// -------------------------------------------------- //
+
+// Definition of MIP cut
+double EvalMIPCut(treevars *vars, int i_tr, std::vector<CC1piPlotVars> *MIPCutVars){
+   std::vector<CC1piPlotVars> MIPCutVars_tmp;
+   // Get MIP cuts
+   if (MIPCutVars == nullptr){
+      // std::cout << "Getting MIP Cut vars from scratch" << std::endl;
+      MIPCutVars_tmp = GetMIPCutVars(vars);
+   }
+   else{
+      MIPCutVars_tmp = (*MIPCutVars);
+   }
+   // std::cout << "Got MIP Cut vars" << std::endl;
+
+   return doEvalMIPCut(MIPCutVars_tmp, vars, i_tr);
 };
 
 #endif
